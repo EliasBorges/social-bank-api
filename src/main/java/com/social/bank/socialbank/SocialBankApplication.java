a04040b1547186355eab9b473b9ff9778747a6f7
Elias Borges <eliasborges@unipam.edu.br>
2021-04-30 20:34:29 -0300

    Estrutura inicial do projeot

---
+++ src/main/java/com/social/bank/socialbank/SocialBankApplication.java
@@ -0,0 +1,13 @@
+package com.social.bank.socialbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBankApplication.class, args);
+	}
+
+}
